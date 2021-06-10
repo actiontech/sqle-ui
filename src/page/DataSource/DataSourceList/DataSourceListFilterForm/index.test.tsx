03c7f6eecee451e31c134fb255a8e0f164229ca7
Rain-1214 <rainwh12138@gmail.com>
2021-06-10 10:54:48 +0800

    test: add unit test for data source list page

---
+++ src/page/DataSource/DataSourceList/DataSourceListFilterForm/index.test.tsx
@@ -0,0 +1,114 @@
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { isEqual } from 'lodash';
+import DataSourceListFilterForm from '.';
+import {
+  mockUseInstance,
+  mockUseRole,
+  mockUseRuleTemplate,
+} from '../../../../testUtils/mockRequest';
+
+describe('DataSource/DataSourceList/DataSourceListFilterForm', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+    mockUseInstance();
+    mockUseRuleTemplate();
+    mockUseRole();
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+    jest.clearAllMocks();
+    jest.clearAllTimers();
+  });
+
+  test('should match snapshot', async () => {
+    const { container } = render(
+      <DataSourceListFilterForm submit={jest.fn()} />
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  test('should submit filter info when user click search button', async () => {
+    const submitFn = jest.fn();
+    render(<DataSourceListFilterForm submit={submitFn} />);
+    await waitFor(() => {
+      jest.advanceTimersByTime(3000);
+    });
+    fireEvent.mouseDown(
+      screen.getByLabelText('dataSource.dataSourceForm.name')
+    );
+    await waitFor(() => {
+      jest.advanceTimersByTime(0);
+    });
+    const option = screen.getAllByText('instance1')[1];
+    fireEvent.click(option);
+    fireEvent.click(screen.getByText('common.search'));
+
+    expect(submitFn).toBeCalledTimes(1);
+    expect(submitFn).toBeCalledWith({
+      filter_instance_name: 'instance1',
+    });
+
+    fireEvent.click(screen.getByText('common.reset'));
+    expect(submitFn).toBeCalledTimes(2);
+    expect(submitFn).toBeCalledWith({});
+  });
+
+  test('should reset all filter info when user click reset button', async () => {
+    const submitFn = jest.fn();
+    render(<DataSourceListFilterForm submit={submitFn} />);
+    await waitFor(() => {
+      jest.advanceTimersByTime(3000);
+    });
+    fireEvent.mouseDown(
+      screen.getByLabelText('dataSource.dataSourceForm.name')
+    );
+    await waitFor(() => {
+      jest.advanceTimersByTime(0);
+    });
+    const option = screen.getAllByText('instance1')[1];
+    fireEvent.click(option);
+    fireEvent.click(screen.getByText('common.search'));
+
+    expect(submitFn).toBeCalledTimes(1);
+    expect(submitFn).toBeCalledWith({
+      filter_instance_name: 'instance1',
+    });
+  });
+
+  test('should reset a part of fields when user collapse filter', async () => {
+    const submitFn = jest.fn();
+    render(<DataSourceListFilterForm submit={submitFn} />);
+    await waitFor(() => {
+      jest.advanceTimersByTime(3000);
+    });
+
+    expect(screen.queryByText('common.collapse')).not.toBeInTheDocument();
+    fireEvent.click(screen.getByText('common.expansion'));
+    expect(screen.queryByText('common.expansion')).not.toBeInTheDocument();
+
+    fireEvent.mouseDown(
+      screen.getByLabelText('dataSource.dataSourceForm.ruleTemplate')
+    );
+    await waitFor(() => {
+      jest.advanceTimersByTime(0);
+    });
+    const option = screen.getAllByText('rule_template_name1')[1];
+    fireEvent.click(option);
+
+    fireEvent.click(screen.getByText('common.collapse'));
+    expect(screen.queryByText('common.expansion')).toBeInTheDocument();
+
+    expect(submitFn).toBeCalledTimes(1);
+    expect(
+      isEqual(submitFn.mock.calls[0][0], {
+        filter_instance_name: undefined,
+        filter_db_host: undefined,
+        filter_db_port: undefined,
+        filter_db_user: undefined,
+        filter_rule_template_name: undefined,
+        filter_role_name: undefined,
+      })
+    ).toBe(true);
+  });
+});
---
+++ src/page/DataSource/UpdateDataSource/index.test.tsx
@@ -0,0 +1,185 @@
+import { fireEvent, waitFor, screen } from '@testing-library/react';
+import UpdateDataSource from '.';
+import instance from '../../../api/instance';
+import {
+  renderWithThemeAndRouter,
+  renderWithThemeAndServerRouter,
+} from '../../../testUtils/customRender';
+import {
+  mockUseRole,
+  mockUseRuleTemplate,
+  resolveThreeSecond,
+} from '../../../testUtils/mockRequest';
+import { useParams } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+
+jest.mock('react-router', () => {
+  return {
+    ...jest.requireActual('react-router'),
+    useParams: jest.fn(),
+  };
+});
+
+describe('UpdateDataSource', () => {
+  const useParamsMock: jest.Mock = useParams as jest.Mock;
+
+  beforeEach(() => {
+    jest.useFakeTimers();
+    useParamsMock.mockReturnValue({ instanceName: '1' });
+    mockUseRuleTemplate();
+    mockUseRole();
+    mockGetInstance();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.clearAllTimers();
+    jest.useRealTimers();
+  });
+
+  const mockUpdateInstanceRequest = () => {
+    const spy = jest.spyOn(instance, 'updateInstanceV1');
+    spy.mockImplementation(() => resolveThreeSecond(undefined));
+    return spy;
+  };
+
+  const mockGetInstance = () => {
+    const spy = jest.spyOn(instance, 'getInstanceV1');
+    spy.mockImplementation(() =>
+      resolveThreeSecond({
+        instance_name: 'db1',
+        db_host: '20.20.20.2',
+        db_port: '3306',
+        db_user: 'root',
+        desc: '',
+        workflow_template_name: 'default',
+      })
+    );
+    return spy;
+  };
+
+  test('should render data source form', async () => {
+    const { container } = renderWithThemeAndRouter(
+      <UpdateDataSource />,
+      undefined
+    );
+    expect(container).toMatchSnapshot();
+
+    await waitFor(() => {
+      jest.advanceTimersByTime(3000);
+    });
+    expect(container).toMatchSnapshot();
+  });
+
+  test('should send update instance request when click submit', async () => {
+    const updateSpy = mockUpdateInstanceRequest();
+    const history = createMemoryHistory();
+    history.push('/data/update/db1');
+    renderWithThemeAndServerRouter(<UpdateDataSource />, undefined, {
+      history,
+    });
+
+    await waitFor(() => {
+      jest.advanceTimersByTime(3000);
+    });
+
+    fireEvent.input(screen.getByLabelText('dataSource.dataSourceForm.name'), {
+      target: { value: 'instance_name1' },
+    });
+    fireEvent.input(
+      screen.getByLabelText('dataSource.dataSourceForm.describe'),
+      {
+        target: { value: 'desc1' },
+      }
+    );
+    fireEvent.input(screen.getByLabelText('dataSource.dataSourceForm.ip'), {
+      target: { value: '1.1.1.1' },
+    });
+    fireEvent.input(screen.getByLabelText('dataSource.dataSourceForm.port'), {
+      target: { value: 4444 },
+    });
+    fireEvent.input(screen.getByLabelText('dataSource.dataSourceForm.user'), {
+      target: { value: 'root' },
+    });
+    fireEvent.input(
+      screen.getByLabelText('dataSource.dataSourceForm.password'),
+      {
+        target: { value: '123456' },
+      }
+    );
+    fireEvent.mouseDown(
+      screen.getByLabelText('dataSource.dataSourceForm.role')
+    );
+
+    await screen.findAllByText('role_name1');
+    const allRoleOptions = screen.getAllByText('role_name1');
+    const roleOption = allRoleOptions[1];
+    expect(roleOption).toHaveClass('ant-select-item-option-content');
+    fireEvent.click(roleOption);
+
+    fireEvent.mouseDown(
+      screen.getByLabelText('dataSource.dataSourceForm.ruleTemplate')
+    );
+    await screen.findAllByText('rule_template_name1');
+    const allInstanceOptions = screen.getAllByText('rule_template_name1');
+    const instanceOption = allInstanceOptions[1];
+    expect(instanceOption).toHaveClass('ant-select-item-option-content');
+    fireEvent.click(instanceOption);
+
+    await waitFor(() => {
+      fireEvent.click(screen.getByText('common.submit'));
+    });
+
+    expect(updateSpy).toBeCalledTimes(1);
+    expect(updateSpy).toBeCalledWith({
+      db_host: '1.1.1.1',
+      db_password: '123456',
+      db_port: '4444',
+      db_user: 'root',
+      desc: 'desc1',
+      instance_name: 'instance_name1',
+      role_name_list: ['role_name1'],
+      rule_template_name_list: ['rule_template_name1'],
+    });
+
+    await waitFor(() => {
+      jest.advanceTimersByTime(3000);
+    });
+    expect(
+      screen.getByText('dataSource.updateDatabase.updateDatabaseSuccess')
+    ).not.toBeNull();
+
+    expect(history.location.pathname).toBe('/data');
+  });
+
+  // test('should reset all fields when click reset button', async () => {
+  //   renderWithTheme(<UpdateDataSource />);
+
+  //   await waitFor(() => {
+  //     jest.advanceTimersByTime(3000);
+  //   });
+
+  //   fireEvent.input(screen.getByLabelText('dataSource.dataSourceForm.name'), {
+  //     target: { value: 'instance_name1' },
+  //   });
+  //   expect(screen.getByLabelText('dataSource.dataSourceForm.name')).toHaveValue(
+  //     'instance_name1'
+  //   );
+
+  //   fireEvent.input(screen.getByLabelText('dataSource.dataSourceForm.port'), {
+  //     target: { value: 4444 },
+  //   });
+  //   expect(screen.getByLabelText('dataSource.dataSourceForm.port')).toHaveValue(
+  //     '4444'
+  //   );
+
+  //   fireEvent.click(screen.getByText('common.reset'));
+
+  //   expect(screen.getByLabelText('dataSource.dataSourceForm.name')).toHaveValue(
+  //     ''
+  //   );
+  //   expect(screen.getByLabelText('dataSource.dataSourceForm.port')).toHaveValue(
+  //     '3306'
+  //   );
+  // });
+});
