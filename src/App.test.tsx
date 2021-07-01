d6f215e8389c37a4160b624e640590e2d35e41f9
Rain-1214 <rainwh12138@gmail.com>
2021-07-01 19:03:52 +0800

    [test]: fix error unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -56,7 +56,7 @@ describe('App test', () => {
     const Switch = Nav.find('Switch');
     expect(Switch.length).toBe(1);
     const routes = Switch.find('Route');
-    expect(routes.length).toBe(11);
+    expect(routes.length).toBe(12);
     const redirect = Switch.find('Redirect');
     expect(redirect.length).toBe(1);
     expect(redirect.at(0).prop('to')).toBe('/');
