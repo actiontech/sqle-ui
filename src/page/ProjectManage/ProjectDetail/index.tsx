9fbe1b9ed6b77faa82abd86343b2a6f46c5f055f
lizhensheng <lizhensheng@actionsky.com>
2022-11-15 10:32:35 +0800

    [chore]: add todo notes, follow up

--- src/page/ProjectManage/ProjectDetail/index.tsx
+++ src/page/ProjectManage/ProjectDetail/index.tsx
@@ -2,6 +2,7 @@ import { useSelector } from 'react-redux';
 import { IReduxState } from '../../../store';
 import ProjectDetail from './ProjectDetail';
 
+//todo 后续需要换个方式实现当前项目名称的保存与获取.
 export const useCurrentProjectName = () => {
   const projectName = useSelector((state: IReduxState) => {
     return state.projectManage.selectProject?.name ?? '';
