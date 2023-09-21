2454a1342afb95182151f6f7db0f082bc9e209ce
lizhensheng <lizhensheng@actionsky.com>
2023-09-21 14:55:51 +0800

    [chore]: clear martin bottom for EditText

--- src/components/EditText/EditText.tsx
+++ src/components/EditText/EditText.tsx
@@ -11,6 +11,7 @@ const EditText: React.FC<EditTextProps> = (props) => {
 
   return (
     <Typography.Paragraph
+      className="clear-margin-bottom"
       {...otherProps}
       editable={{
         ...editable,
